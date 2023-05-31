0afc9975420f2e093203120d9d07b13ffba029a6
SolGaleano <130614063+SolGaleano@users.noreply.github.com>
2023-05-30 21:12:14 -0300

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,10 @@
+var numero = prompt ("Ingrese su edad");
+function mayormenor (){
+
+ if (numero<"18"){
+        alert("Ingresa bajo su propia responsabilidad");
+    } else{
+        alert ("Puede ingresar");
+    }
+}
+console.log(mayormenor());
